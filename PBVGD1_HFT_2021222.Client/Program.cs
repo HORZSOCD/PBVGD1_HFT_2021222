01640c746c417e9ad02d99c96b3ebd11d91354a9
Horváth Zsolt <h.zsolt1224@gmail.com>
2022-04-17 16:45:02 +0200

    Language corrections

--- PBVGD1_HFT_2021222.Client/Program.cs
+++ PBVGD1_HFT_2021222.Client/Program.cs
@@ -1,9 +1,8 @@
 ﻿using System;
-using System.Linq;
 
 namespace PBVGD1_HFT_2021222.Client
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
