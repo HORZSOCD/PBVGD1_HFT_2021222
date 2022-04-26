f103a2cf3e633b6b9ed85313d20dd91fc941c70c
Horváth Zsolt <h.zsolt1224@gmail.com>
2022-04-26 14:31:19 +0200

    SportLogic with Interface created

---
+++ PBVGD1_HFT_2021222.Logic/ISportLogic.cs
@@ -0,0 +1,14 @@
+﻿using PBVGD1_HFT_2021222.Models;
+using System.Linq;
+
+namespace PBVGD1_HFT_2021222.Logic
+{
+    public interface ISportLogic
+    {
+        void Create(Sport item);
+        void Delete(int id);
+        Sport Read(int id);
+        IQueryable<Sport> ReadAll();
+        void Update(Sport item);
+    }
+}
---
+++ PBVGD1_HFT_2021222.Logic/SportLogic.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Linq;
+using PBVGD1_HFT_2021222.Models;
+using PBVGD1_HFT_2021222.Repository;
+
+namespace PBVGD1_HFT_2021222.Logic
+{
+    public class SportLogic : ISportLogic
+    {
+        IRepository<Sport> sportRepo;
+
+        public SportLogic(IRepository<Sport> sportRepo)
+        {
+            this.sportRepo = sportRepo;
+        }
+        public void Create(Sport item)
+        {
+            this.sportRepo.Create(item);
+        }
+
+        public void Delete(int id)
+        {
+            this.sportRepo.Delete(id);
+        }
+
+        public Sport Read(int id)
+        {
+            var sport = this.sportRepo.Read(id);
+            if (sport == null)
+                throw new ArgumentException("Sport does not exist.");
+            return sport;
+        }
+
+        public IQueryable<Sport> ReadAll()
+        {
+            return this.sportRepo.ReadAll();
+        }
+
+        public void Update(Sport item)
+        {
+            this.sportRepo.Update(item);
+        }
+    }
+
+}
